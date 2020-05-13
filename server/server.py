d65e8e3b7b6b458ecd275acc8dcfaf14fbaa96da
AFronto <andrasfronto@gmail.com>
2020-05-13 14:38:47 +0200

    server netinterface test done

---
+++ server/server.py
@@ -0,0 +1,6 @@
+from netsim.netinterface import network_interface
+
+netif = network_interface("../netsim", 'A')		
+status, msg = netif.receive_msg(blocking=True)		
+print(msg)
+netif.send_msg('C', 'At jott')
